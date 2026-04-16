17b1534bfe5ebe858347f14738bfb0a87ac46c33
Nick Podratz <nick.podratz@syntax-institut.de>
2026-04-16 11:05:03 +0200

    Setup Tailwind

--- src/WelcomeMessage.jsx
+++ src/WelcomeMessage.jsx
@@ -2,7 +2,7 @@ import styles from './Highlights.module.css'
 
 function WelcomeMessage() {
     return (
-        <p className={styles.important}>
+        <p className={`${styles.important} bg-amber-200`}>
             We are delighted to welcome you on our platform.
         </p>
     )
