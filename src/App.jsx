76c776dad951822b10d67165568d8a23831f554d
Nick Podratz <nick.podratz@syntax-institut.de>
2026-04-16 11:20:31 +0200

    Add sizeclass overrides

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Center from './Center'
 import HelloTitle from './HelloTitle'
 import WelcomeMessage from './WelcomeMessage'
-import hightlightStyles from './Highlights.module.css'
+import Copyright from './Copyright'
 
 function App() {
   return (
     <Center>
       <HelloTitle name="Syntax" />
       <WelcomeMessage />
-      <p className={hightlightStyles.important}>Copyright by Syntax</p>
+      <Copyright />
     </Center>
   )
 }
---
+++ src/Copyright.jsx
@@ -0,0 +1,5 @@
+export default function Copyright() {
+    return (
+        <p className="text-red-600 md:text-white md:bg-red-600 xl:bg-red-900">Copyright by Syntax</p>
+    )
+}
