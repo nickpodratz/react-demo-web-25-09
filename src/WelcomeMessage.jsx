faf1c15d187df2059a46aef237ab190f40e9524c
Nick Podratz <nick.podratz@syntax-institut.de>
2026-04-17 11:32:50 +0200

    Embed image

--- src/WelcomeMessage.jsx
+++ src/WelcomeMessage.jsx
@@ -1,9 +1,10 @@
 import styles from './Highlights.module.css'
+import heroImage from './assets/hero.png'
 
 function WelcomeMessage() {
     return (
         <p className={`${styles.important} bg-amber-200`}>
-            We are delighted to welcome you on our platform.
+            <img src={heroImage} height="50px" width="50px" /> We are delighted to welcome you on our platform.
         </p>
     )
 }
