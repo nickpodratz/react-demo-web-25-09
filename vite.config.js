7416ddee335b700575bc4523129bae37a8f98c97
Nick Podratz <nick.podratz@syntax-institut.de>
2026-04-21 13:04:03 +0200

    Add ngrok plugin

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
+import { ngrok } from 'vite-plugin-ngrok'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwind()
-  ],
-  server: {
-    allowedHosts: [
-      "*.ngrok-free.dev"
-    ]
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      tailwind(),
+      command === 'serve' && ngrok({
+        authtoken: env.NGROK_AUTH_TOKEN
+      })
+    ],
+    server: {
+      allowedHosts: ["*.ngrok-free.dev"]
+    }
   }
 })
