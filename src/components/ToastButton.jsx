7a6e9a7d764bc329adb1f9c1cdbf141fcec5c9ad
Nick Podratz <nick.podratz@syntax-institut.de>
2026-04-21 12:08:56 +0200

    Remodel button tailwind styling

--- src/components/ToastButton.jsx
+++ src/components/ToastButton.jsx
@@ -2,7 +2,7 @@ import toast from "react-hot-toast";
 
 export default function ToastButton() {
     return (
-        <button className="button border-0 text-white bg-green-500" onClick={() => toast.success("Notifications working!")}>
+        <button className="btn-primary" onClick={() => toast.success("Notifications working!")}>
             Send Notification    
         </button>
     )
