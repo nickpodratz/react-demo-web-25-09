b41e3f6a0e253cf2840baae53f23bddef3516d07
Nick Podratz <nick.podratz@syntax-institut.de>
2026-04-21 11:58:36 +0200

    Fix blog rendering

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -4,13 +4,17 @@ export default function Blog() {
     return (
         <>
             <h1>Blog</h1>
-            <ul>
+            <ol className="list-decimal pl-6">
                 <li>
                     <Link to="/blog/first-article">First Article</Link>
+                </li>
+                <li>
                     <Link to="/blog/second-article">Second Article</Link>
+                </li>
+                <li>
                     <Link to="/blog/third-article">Third Article</Link>
                 </li>
-            </ul>
+            </ol>
         </>
     )
 }
