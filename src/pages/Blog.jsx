7da6a375b3c447b50bf4889dca650643ca0279f0
Nick Podratz <nick.podratz@syntax-institut.de>
2026-04-22 11:36:58 +0200

    Create dynamic blog list

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom"
 
 export default function Blog() {
+    const posts = [
+        {title: "First Post", slug: "first-post"},
+        {title: "Second Post", slug: "second-post"},
+        {title: "Third Post", slug: "third-post"},
+    ]
+
     return (
         <>
             <h1>Blog</h1>
             <ol>
-                <li>
-                    <Link to="/blog/first-post">First Post</Link>
-                </li>
-                <li>
-                    <Link to="/blog/second-post">Second Post</Link>
-                </li>
-                <li>
-                    <Link to="/blog/third-post">Third Post</Link>
-                </li>
+                {posts.map(post => (
+                    <li>
+                        <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+                    </li>
+                ))}
             </ol>
         </>
     )
