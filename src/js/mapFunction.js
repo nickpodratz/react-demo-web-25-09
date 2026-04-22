a085d96b7fe4ff7da070a5e2c33945398f9db0c8
Nick Podratz <nick.podratz@syntax-institut.de>
2026-04-22 10:38:11 +0200

    Add map examples

--- src/js/mapFunction.js
+++ src/js/mapFunction.js
@@ -15,6 +15,23 @@ function makeId(input) {
     return capitalize(shorten(input))
 }
 
-const newNames = names.map(makeId)
+// (str, num) -> str
+const toNumberedName = (name, index) => {
+    const prefix = index + 1
+    const nameSuffix = makeId(name)
+    return `${prefix}. ${nameSuffix}`
+}
 
-console.log(newNames)
+const newNames1 = names.map(makeId)
+const newNames2 = names.map(toNumberedName)
+
+console.log(newNames2)
+
+
+const output = [1, 2, 3, 4].map(x => x + 4)
+
+console.log(output)
+
+
+const price = 7.15
+console.log(Math.ceil(price))
